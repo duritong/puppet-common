7820051c671cf47043bb15ad646e32c6a73d61a6
David Schmitt <david@schmitt.edv-bus.at>
2008-06-05 16:48:00 +0200

    remove one more $servername

--- manifests/defines/modules_dir.pp
+++ manifests/defines/modules_dir.pp
@@ -16,7 +16,7 @@ define modules_dir (
 	} else {
 		file {
 			"/var/lib/puppet/modules/${name}":
-				source => [ "puppet:///${name}/modules_dir", "puppet://$servername/common/empty"],
+				source => [ "puppet:///${name}/modules_dir", "puppet:///common/empty"],
 				checksum => mtime,
 				recurse => true, purge => true, force => true,
 				mode => $mode, owner => $owner, group => $group;
