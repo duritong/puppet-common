3a65dd853506db5d276e4162e6bd6920950fb21b
David Schmitt <david@schmitt.edv-bus.at>
2008-08-25 16:45:29 +0200

    improve documentation and function naming

--- plugins/puppet/parser/functions/basename.rb
+++ plugins/puppet/parser/functions/basename.rb
@@ -1,7 +1,16 @@
-# get the basename of the given filename
+# basename(string) : string
+# basename(string[]) : string[]
+#
+# Returns the last component of the filename given as argument, which must be
+# formed using forward slashes (``/..) regardless of the separator used on the
+# local file system.
 module Puppet::Parser::Functions
 	newfunction(:basename, :type => :rvalue) do |args|
-		File.basename(args[0])
+		if args[0].is_a?(Array)
+			args.collect do |a| File.basename(a) end
+		else
+			File.basename(args[0])
+		end
 	end
 end
 
--- plugins/puppet/parser/functions/dirname.rb
+++ plugins/puppet/parser/functions/dirname.rb
@@ -1,7 +1,16 @@
-# get the directory corresponding to this filename
+# dirname(string) : string
+# dirname(string[]) : string[]
+#
+# Returns all components of the filename given as argument except the last
+# one. The filename must be formed using forward slashes (``/..) regardless of
+# the separator used on the local file system.
 module Puppet::Parser::Functions
 	newfunction(:dirname, :type => :rvalue) do |args|
-		File.dirname(args[0])
+		if args[0].is_a?(Array)
+			args.collect do |a| File.dirname(a) end
+		else
+			File.dirname(args[0])
+		end
 	end
 end
 
