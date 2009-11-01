d75dcc86f862b2fa5b7b72c2958915b4b029c465
mh <mh@immerda.ch>
2009-11-02 00:56:34 +0100

    recursivly updating file path to new 0.25 style

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,7 +7,7 @@ file {
 	# module-local data, e.g. for constructing config files
 	"/var/lib/puppet/modules":
 		ensure => directory,
-		source => "puppet://$server/common/modules/",
+		source => "puppet://$server/modules/common/modules/",
 		ignore => '\.ignore',
 		recurse => true, purge => true, force => true,
 		mode => 0755, owner => root, group => 0;
