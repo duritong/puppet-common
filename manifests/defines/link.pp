d39201f8a224f1dd153818948d72f4c87d3fdf81
mh <mh@immerda.ch>
2009-11-18 23:34:39 +0100

    make the define much easier

--- manifests/defines/link.pp
+++ manifests/defines/link.pp
@@ -7,20 +7,6 @@
 # link { "/usr/bin/convert": target => "/usr/local/bin/convert" }
 # e.g. equivalent to bash# ln -s /usr/bin/convert /usr/local/bin/convert
 
-define link(
-    $target = 'absent'
-) {
-    if ($target != 'absent') {
-      file { "$name":
-            ensure => link,
-            target => "${target}"
-      }
-    }else{
-      file { "$name":
-            ensure => absent,
-            recurse => true,
-            force => true
-      }
-    }
+define link($target = absent) {
+   file{$name: ensure => $target; }
 }
-
